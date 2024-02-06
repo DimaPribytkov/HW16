98dd6476235f1fb853465703d17c52f49c627fa0
ДимаПрибытков <d.pribytkov93@mail.ru>
2024-02-06 22:19:52 +0300

    Проверка на валидность ФИО с помощью регулярного выражения.

--- Task3/src/Main.java
+++ Task3/src/Main.java
@@ -10,6 +10,7 @@ public class Main {
                 System.out.println(true);
             } else {
                 System.out.println("no Valid");
+                break;
             }
         }
     }
