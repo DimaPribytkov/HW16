80ddc42a2e609a28f5d0d5cab12359b075bcb3c1
ДимаПрибытков <d.pribytkov93@mail.ru>
2024-02-06 22:18:35 +0300

    Проверка на валидность ФИО с помощью регулярного выражения

--- Task3/src/Main.java
+++ Task3/src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
+    private static final String VALID_REGEX = "((([А-я]+)|([А-я-]+))\\s){2}(([А-я]+)|([А-я-]+))";
     public static void main(String[] args) {
-        //todo ФИО
+        while (true) {
+            System.out.println("Введите Фамилию Имя Отчество");
+            String fIO = new Scanner(System.in).nextLine();
+            if (fIO.matches(VALID_REGEX)) {
+                System.out.println(true);
+            } else {
+                System.out.println("no Valid");
+            }
+        }
     }
 }
